1c1d2cf79ab51897e4edf2d5255fc160e4e07c5b
RobotMermaid <ondinerangel@gmail.com>
2017-02-28 13:06:34 -0800

    game over

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
-
+//idea : reverse pong option where you have to dodge the ball 'dodge pong'
+//add sound
 
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
 canvas.width  = window.innerWidth-100;
-canvas.height = window.innerHeight-130;
+canvas.height = window.innerHeight-220;
+
+var speedComp = Math.random() * (200-50)-50;
+
 var playerScore = 0;
 var computerScore = 0;
 var scorePlayer = document.getElementById('pScore');
 var scoreComputer = document.getElementById('cScore');
+var message = document.getElementById('message');
 
 var Paddle = function Paddle(xpos, ypos, width, height, speed) {
   this.xpos = xpos;
@@ -22,8 +27,8 @@ var Ball = function Ball(centerX, centerY, radius) {
   this.centerX= centerX;
   this.centerY= centerY;
   this.radius= radius || 20;
-  this.speedX = Math.random() * (6-3) + 3;
-  this.speedY = Math.random() * (6-3) + 3;
+  this.speedX = Math.random() * (7-3) + 3;
+  this.speedY = Math.random() * (7-3) + 3;
 };
 // ******************************************************  Render
 Paddle.prototype.render = function() {
@@ -36,12 +41,11 @@ Paddle.prototype.render = function() {
 Ball.prototype.render = function() {
   context.beginPath();
   context.arc(this.centerX, this.centerY, this.radius, 0, 2 * Math.PI, false);
-  context.fillStyle = 'black';
+  context.fillStyle = 'darkBlue';
   context.fill();
 };
 
 // ******************************************************  Move
-// move the paddle within the canvas space & define speed
 Paddle.prototype.move = function(event) {
   if (event.keyCode === 38 && this.ypos>0) {
     this.ypos -= this.speed;
@@ -59,12 +63,12 @@ Ball.prototype.move = function() {
     this.speedY = this.speedY * -1;
   }
 //going thru the sides
-  if(this.centerX + this.radius > canvas.width) {
+  if(this.centerX + this.radius > canvas.width-5) {
     this.speedX = this.speedX * -1;
     computerScore +=1;
 
     console.log('o no ' + computerScore + " points for the computer"  );
-// increment points and reload game
+
   }
   if(this.centerX - this.radius < 0) {
     this.speedX = this.speedX * -1;
@@ -73,7 +77,6 @@ Ball.prototype.move = function() {
   }
 };
 // // ------------collision-----------------
-var speedComp=10;
 var collisionCheck = function(paddle, axis) {
   var widthheight = axis == "x" ? "width" : "height";
   var centerRect = paddle[axis + "pos"] + paddle[widthheight] / 2;
@@ -85,48 +88,58 @@ var collision = function() {
   var distXcomputer = collisionCheck(computer, "x");
   var distYcomputer = collisionCheck(computer, "y");
 
-  if (distXpaddle <= (paddleR.width/2 + ball.radius) && distYpaddle <= (paddleR.height/2 + ball.radius)) {
-
+  if (distXpaddle < (paddleR.width/2 + ball.radius) && distYpaddle < (paddleR.height/2 + ball.radius)) {
       ball.speedX = ball.speedX * -1;
-      // this works surprisingly enough
-      // hitzone = ball.centerY+20;
-      speedComp = Math.random() * (200-50)-50;
-    console.log("boing" );
+      console.log("boing" );
   };
-  if (distXcomputer <= (computer.width/2 + ball.radius) && distYcomputer <= (computer.height/2 + ball.radius)) {
-    console.log("boop" );
+  if (distXcomputer < (computer.width/2 + ball.radius) && distYcomputer < (computer.height/2 + ball.radius)) {
+      console.log("boop" );
       ball.speedX = ball.speedX * -1;
   };
 };
 
 //
 // ******************************************************  Instances
-var paddleR = new Paddle(canvas.width-25, canvas.height/2-50, 20, 100, 50);
+var paddleR = new Paddle(canvas.width-20, canvas.height/2-50, 20, 100, 50);
 var ball = new Ball(canvas.width/2, canvas.height/2);
-var computer = new Paddle(5, canvas.height/2-50, 20, 100);
+var computer = new Paddle(0, canvas.height/2-50, 20, 100);
+var gamePlaying = false;
 var updateComputer = function() {
   computer.ypos = ball.centerY-speedComp;
   // paddleR.ypos = ball.centerY -50;
 };
 
 var animate = window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame    ||
-        window.oRequestAnimationFrame      ||
-        window.msRequestAnimationFrame     ||
-        function(callback) { window.setTimeout(callback, 1000/60) };
+              window.webkitRequestAnimationFrame ||
+              window.mozRequestAnimationFrame    ||
+              window.oRequestAnimationFrame      ||
+              window.msRequestAnimationFrame     ||
+              function(callback) { window.setTimeout(callback, 1000/60) };
 
 var drawLine = function() {
+  context.setLineDash([20, 20]);
   context.beginPath();
   context.moveTo(canvas.width/2, 0);
+  context.lineWidth = 5;
   context.lineTo(canvas.width/2, canvas.height);
+  context.strokeStyle = 'gray';
   context.stroke();
 };
 
 var updateScore = function() {
   scorePlayer.innerHTML = playerScore;
   scoreComputer.innerHTML = computerScore;
-}
+  if(playerScore >=11 || computerScore >=11) {
+    var winner = playerScore > computerScore ? message.innerHTML="you win!" : message.innerHTML="you loose";
+    message.style.visibility = 'visible';
+    gamePlaying = false;
+    document.getElementById('end-of-time').play()
+
+    playerScore = 0;
+    computerScore = 0;
+
+  };
+};
 // callback for animate function
   function step() {
       context.clearRect(0,0,canvas.width, canvas.height);
@@ -138,7 +151,21 @@ var updateScore = function() {
       computer.render();
       paddleR.render();
       updateScore();
-      animate(step);
+      if (gamePlaying){
+        animate(step);
+      }
+
 }
-    window.addEventListener('keydown', paddleR.move.bind(paddleR));
+
+function keyListener(event) {
+  console.log(event.keyCode);
+  if (event.keyCode == 38 || event.keyCode == 40) {
+    paddleR.move(event);
+  } else if(event.keyCode === 32 && gamePlaying === false) {
+    message.style.visibility = 'hidden';
+    gamePlaying = true;
+    animate(step);
+  }
+}
+    window.addEventListener('keydown', keyListener);
     window.onload = animate(step);
